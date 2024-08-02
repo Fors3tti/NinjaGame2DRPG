6d4b0e0647d73be8c7473a47267e61079b783871
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2024-08-02 18:24:52 -0400

    Created Enemies Damage Players Config

--- NinjaGame2DRPG/Assets/Scripts/Player/PlayerHealth.cs
+++ NinjaGame2DRPG/Assets/Scripts/Player/PlayerHealth.cs
@@ -27,6 +27,7 @@ public class PlayerHealth : MonoBehaviour, IDamageable
         stats.Health -= amount;
         if(stats.Health <= 0f)
         {
+            stats.Health = 0f;
             PlayerDead();
         }
     }
